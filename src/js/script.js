0b9db3ecedec55c246de3312d0d4965403175ad0
Márcio Alexandre <marciotrabalho1701@gmail.com>
2025-06-02 20:49:53 -0300

    botões de slide

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,48 @@
 const corback= window.document.body
 const corfont = window.document.body
+const image = window.document.body
+/* const slide = window.document. */
+
+
+
+
+
+function iniciar(){
+    sliderum.classList.remove('hidden');
+    sliderdois.classList.add('hidden');
+    slidertres.classList.add('hidden');
+}
+
+iniciar()
 
 function clicarcin(){
-    window.document.getElementById(cinza)
     corback.style.background = 'dimgrey'
     corfont.style.color = 'white'
 }
 function clicarbra(){
-    window.document.getElementById(branco)
     corback.style.background = 'white'
     corfont.style.color = 'black'
 }
 function clicarazul(){
-    window.document.getElementById(azul)
     corback.style.background = 'rgb(65, 134, 195)'
     corfont.style.color = 'white'
 }
 
-
-
-
-
-
-
-
-
+function slide1(){
+    sliderum.classList.remove('hidden');
+    sliderdois.classList.add('hidden');
+    slidertres.classList.add('hidden');
+}
+function slide2(){
+    sliderum.classList.add('hidden');
+    sliderdois.classList.remove('hidden');
+    slidertres.classList.add('hidden');
+}
+function slide3(){
+    sliderum.classList.add('hidden');
+    sliderdois.classList.add('hidden');
+    slidertres.classList.remove('hidden');
+}
 
 
 
