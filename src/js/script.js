43464290a2d05869b1c814138185b09924ad6db0
Márcio Alexandre <marciotrabalho1701@gmail.com>
2025-06-02 12:13:25 -0300

    Criação dos botões de mudar backgroundcolor

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+const corback= window.document.body
+const corfont = window.document.body
+
+function clicarcin(){
+    window.document.getElementById(cinza)
+    corback.style.background = 'dimgrey'
+    corfont.style.color = 'white'
+}
+function clicarbra(){
+    window.document.getElementById(branco)
+    corback.style.background = 'white'
+    corfont.style.color = 'black'
+}
+function clicarazul(){
+    window.document.getElementById(azul)
+    corback.style.background = 'rgb(65, 134, 195)'
+    corfont.style.color = 'white'
+}
 
 
 
